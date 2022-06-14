cae306578f25868a74ab0a8de74a311c899ac3d0
metal-reigns <isniper19931221@yahoo.com>
2022-06-14 15:21:03 +0300

    doubleMerge

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -2,28 +2,66 @@ import React, { useState } from "react";
 import "./CostForm.css";
 
 function CostForm() {
-  const [name, setName] = useState("");
-  const [amount, setAmount] = useState("");
-  const [date, setDate] = useState("");
+  const [inputName, setInputName] = useState("");
+  const [inputAmount, setInputAmount] = useState("");
+  const [inputDate, setInputDate] = useState("");
+
+  //   const [userInput, setUserInput] = useState({
+  //     name: "",
+  //     amount: "",
+  //     date: "",
+  //   });
 
   const nameChangeHandler = (event) => {
-    setName(event.target.value);
+    setInputName(event.target.value);
+    //     setUserInput({
+    //       ...userInput,
+    //       name: event.target.value,
+    //     });
+    //     setUserInput((previousState) => {
+    //       return {
+    //         ...previousState,
+    //         name: event.target.value,
+    //       };
+    //     });
   };
 
   const amountChangeHandler = (event) => {
-    setAmount(event.target.value);
+    setInputAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   amount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    setDate(event.target.value);
+    setInputDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   date: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const costData = {
+      name: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate),
+    };
+    console.log(costData);
+    setInputName("");
+    setInputAmount("");
+    setInputDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-cost__controls">
         <div className="new-cost__control">
           <label>Название</label>
-          <input type="text" onChange={nameChangeHandler} />
+          <input type="text" value={inputName} onChange={nameChangeHandler} />
         </div>
         <div className="new-cost__control">
           <label>Сумма</label>
@@ -31,6 +69,7 @@ function CostForm() {
             type="number"
             min="0.01"
             step="0.01"
+            value={inputAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -40,6 +79,7 @@ function CostForm() {
             type="date"
             min="2022-01-01"
             step="2022-12-31"
+            value={inputDate}
             onChange={dateChangeHandler}
           />
         </div>
