85e1d83e7354faf6b37acb9cc3f6f46e232dd268
metal-reigns <isniper19931221@yahoo.com>
2022-06-15 12:51:22 +0300

    Statistic

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import React, { useState } from "react";
 import CostList from "./CostList";
+import CostsDiagram from "./CostsDiagram";
 
 function Costs(props) {
   const [selectedYear, setSelectedYear] = useState("2021");
@@ -19,6 +20,7 @@ function Costs(props) {
     <div>
       <Card className="costs">
         <CostsFilter onChangeYear={yearChangeHandler} year={selectedYear} />
+        <CostsDiagram costs={filteredCosts} />
         <CostList costs={filteredCosts} />
       </Card>
     </div>
